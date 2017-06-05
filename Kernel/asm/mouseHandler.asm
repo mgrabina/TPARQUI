a23c11a1eb17e1b12126f7070667b10079af6da2
Martin Grabina <mgrabina@itba.edu.ar>
2017-06-05 15:03:11 -0300

    asasd

---
+++ Kernel/asm/mouseHandler.asm
@@ -0,0 +1,11 @@
+section .text
+GLOBAL getMouseInfo
+
+getMouseInfo:
+	push rbp
+	mov rbp, rsp
+	xor rax,rax
+	in al,60h 	
+	mov rsp, rbp
+	pop rbp
+	ret
--- Kernel/mouseDriver.c
+++ Kernel/mouseDriver.c
@@ -1,4 +1,15 @@
-mouseHandler(){
+#include <stdint.h>
+#include <string.h>
+#include <lib.h>
+#include <moduleLoader.h>
+#include <naiveConsole.h>
+#include <interruptions.h>
+
+int getMouseInfo();
+
+void mouseHandler();
+
+void mouseHandler(){
 	//Info: http://www.computer-engineering.org/ps2mouse/
 	//				Bit 7	Bit 6		Bit 5		Bit 4		Bit 3		Bit 2		Bit 1		Bit 0
 	//  Byte 1	Y overflow	X overflow	Y sign bit	X sign bit	Always 1	Middle Btn	Right Btn	Left Btn
