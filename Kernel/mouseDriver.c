32a7b8c8d11a1f26234f0fca79dbca047618a60f
Martin Ezequiel Grabina <mgrabina@MGRABINA.mgrabina.github.io>
2017-06-07 19:09:17 -0300

    HAY MOUSE, solo falta manejarlo para mostrarlo

--- Kernel/mouseDriver.c
+++ Kernel/mouseDriver.c
@@ -12,26 +12,49 @@ unsigned char inc(unsigned char);
 void mouseHandler();
 void startMouse();
 
-void wait(){
 
-}
 void startMouse(){
 	ncPrint("[Inicializando mouse..]");
 	ncNewline();
-	while(inc(0x64)==0x01);
+	waitW();
+	outc(0x64, 0xA8);
+	waitW();
 	outc(0x64, 0x20);
+	waitR();
 	unsigned char c = inc(0x60);
-	c = c | 0x02;
-	c = c & 0xEF;
-	while(inc(0x64)==0x01);	
+	c = c | 2;
+	waitW();
+	outc(0x64, 0x60);	
+	waitW();
 	outc(0x60, c);
-	while(inc(0x64)==0x01);
-	outc(0x64, 0x60);
-	while(inc(0x64)==0x01);
-	outc(0x64, 0xFF);
+	waitW();
+	outc(0x60, 0xF6);
+	waitW();
+	outc(0x60, 0xF4);
 	ncPrint("[Mouse inicializado.]");
 }
 
+void waitW(){
+	int c = 100000;
+	while(c!=0){
+		if((inc(0x64)&1) == 1)return;		
+		c=c-1;
+	}
+}
+void waitR(){
+	int c = 100000;
+	while(c!=0){
+		if((inc(0x64)&2) == 0)return;		
+		c=c-1;
+	}
+}
+
+int GMI(){
+	waitR();
+	inc(0x60);
+}
+
+static int ciclo = 0;
 void mouseHandler(){
 	//Info: http://www.computer-engineering.org/ps2mouse/
 	//				Bit 7	Bit 6		Bit 5		Bit 4		Bit 3		Bit 2		Bit 1		Bit 0
@@ -40,36 +63,26 @@ void mouseHandler(){
 	//  char 3	Y movement (Entero)
 	//Int -> 4 chars
 	//Proceso la informacion
-	ncPrint("Entro al mouse handler de c");
-	int info = getMouseInfo();
-	int char1 = (info >> (8*0)) & 0xff;
-	int xmovement = (info >> (8*1)) & 0xff;
-	int ymovement = (info >> (8*2)) & 0xff; //Chekear si el que se agrega es el primero o el ultimo
-	int yoverflow = ((char1 >> 7)  & 0x01);
-	int xoverflow = ((char1 >> 6)  & 0x01);
-	int ysign = ((char1 >> 5)  & 0x01);
-	int xsign = ((char1 >> 4)  & 0x01);
-	//int xoverflow = ((char1 >> 3)  & 0x01);
-	int middleButton = ((char1 >> 2)  & 0x01);
-	int rightButton = ((char1 >> 2)  & 0x01);
-	int leftButton = ((char1 >> 2)  & 0x01);
+	char info[3];
+	info[ciclo++] = GMI(); 	
+	if(ciclo == 3){
+		ciclo=0;
+		
+		//MANEJAR CAMBIOS DE UBICACION
+		ncPrintDec(info[1]);
+		ncPrintDec(info[2]);		
 
-	if(rightButton){
-		//Click derecho
-		ncPrint("[Click derecho]");
-	}
-	if(middleButton){
-		//Click central
-		ncPrint("[Click central]");
-	}
-	if(leftButton){
-		//Click izquierdo
-		ncPrint("[Click izquierdo]");
-	}
-	//Mostrar movimientos
-	ncPrint("Movimientos: (");
-	ncPrint(xmovement);
-	ncPrint(" , ");
-	ncPrint(ymovement);
-	ncPrint(")");
+		if(info[0] & 0x2){
+			//Click derecho
+			ncPrint("[Click derecho]");
+		}
+		if(info[0] & 0x4){
+			//Click central
+			ncPrint("[Click central]");
+		}
+		if(info[0] & 0x1){
+			//Click izquierdo
+			ncPrint("[Click izquierdo]");
+		}
+	}	
 }
