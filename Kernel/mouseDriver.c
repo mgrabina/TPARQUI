9af74282c61ee8fb2b58bef6dd87963c8a02d13f
Martin Grabina <mgrabina@itba.edu.ar>
2017-06-07 16:03:05 -0300

    asd

--- Kernel/mouseDriver.c
+++ Kernel/mouseDriver.c
@@ -6,8 +6,8 @@
 #include <interruptions.h>
 
 int getMouseInfo();
-void outc(BYTE, BYTE);
-BYTE inc(BYTE);
+void outc(byte, byte);
+byte inc(byte);
 
 void mouseHandler();
 void startMouse();
@@ -15,9 +15,9 @@ void startMouse();
 void startMouse(){
 	ncPrint("[Inicializando mouse..] \n");
 	outc(0x64, 0x20);
-	BYTE byte = inc(0x60);
-	byte = byte || 0x02;
-	byte = byte && 0xEF;
+	byte byte = inc(0x60);
+	byte = byte | 0x02;
+	byte = byte & 0xEF;
 	outc(0x60, byte);
 	outc(0x64, 0x60);
 	//outc(0x64, 0xFF);
@@ -27,24 +27,24 @@ void startMouse(){
 void mouseHandler(){
 	//Info: http://www.computer-engineering.org/ps2mouse/
 	//				Bit 7	Bit 6		Bit 5		Bit 4		Bit 3		Bit 2		Bit 1		Bit 0
-	//  Byte 1	Y overflow	X overflow	Y sign bit	X sign bit	Always 1	Middle Btn	Right Btn	Left Btn
-	//  Byte 2	X movement (Entero)
-	//  Byte 3	Y movement (Entero)
-	//Int -> 4 Bytes
+	//  byte 1	Y overflow	X overflow	Y sign bit	X sign bit	Always 1	Middle Btn	Right Btn	Left Btn
+	//  byte 2	X movement (Entero)
+	//  byte 3	Y movement (Entero)
+	//Int -> 4 bytes
 	//Proceso la informacion
 	ncPrint("Entro al mouse handler de c");
 	int info = getMouseInfo();
-	int Byte1 = (info >> (8*0)) & 0xff;
+	int byte1 = (info >> (8*0)) & 0xff;
 	int xmovement = (info >> (8*1)) & 0xff;
 	int ymovement = (info >> (8*2)) & 0xff; //Chekear si el que se agrega es el primero o el ultimo
-	int yoverflow = ((Byte1 >> 7)  & 0x01);
-	int xoverflow = ((Byte1 >> 6)  & 0x01);
-	int ysign = ((Byte1 >> 5)  & 0x01);
-	int xsign = ((Byte1 >> 4)  & 0x01);
-	//int xoverflow = ((Byte1 >> 3)  & 0x01);
-	int middleButton = ((Byte1 >> 2)  & 0x01);
-	int rightButton = ((Byte1 >> 2)  & 0x01);
-	int leftButton = ((Byte1 >> 2)  & 0x01);
+	int yoverflow = ((byte1 >> 7)  & 0x01);
+	int xoverflow = ((byte1 >> 6)  & 0x01);
+	int ysign = ((byte1 >> 5)  & 0x01);
+	int xsign = ((byte1 >> 4)  & 0x01);
+	//int xoverflow = ((byte1 >> 3)  & 0x01);
+	int middleButton = ((byte1 >> 2)  & 0x01);
+	int rightButton = ((byte1 >> 2)  & 0x01);
+	int leftButton = ((byte1 >> 2)  & 0x01);
 
 	if(rightButton){
 		//Click derecho
